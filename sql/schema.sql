95f377bc313d5529cfbabeccb6b16ffc3e546a18
johnkyle <johnkyle@umich.edu>
2022-01-30 21:33:21 -0500

    update

--- sql/schema.sql
+++ sql/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE posts(
 
 /* check to see if we need to define the following relationship "username1 follows username2" */
 CREATE TABLE following(
-    username1 VARCHAR( 20) NOT NULL,
+    username1 VARCHAR(20) NOT NULL,
     username2 VARCHAR(20) NOT NULL,
     created DATETIME,
     PRIMARY KEY (username1, username2),
@@ -28,20 +28,20 @@ CREATE TABLE following(
 );
 
 CREATE TABLE comments(
-    commentid INTEGER  PRIMARY KEY AUTOINCREMENT,
-    owner VARCHAR(20) NOT NULL ON DELETE CASCADE,
-    postid INTEGER NOT NULL ON DELETE CASCADE,
+    commentid INTEGER PRIMARY KEY AUTOINCREMENT,
+    owner VARCHAR(20) NOT NULL,
+    postid INTEGER NOT NULL,
     text VARCHAR(1024) NOT NULL,
     created DATETIME,
     FOREIGN KEY (owner) REFERENCES users(username),
-    FOREIGN KEY (postid) REFERENCES posts(postid)
+    FOREIGN KEY (postid) REFERENCES posts(postid) ON DELETE CASCADE
 );
 
 CREATE TABLE likes(
-    likeid INTEGER  PRIMARY KEY,
-    owner VARCHAR(20) NOT NULL ON DELETE CASCADE,
-    postid INTEGER NOT NULL ON DELETE CASCADE,
+    likeid INTEGER PRIMARY KEY,
+    owner VARCHAR(20) NOT NULL,
+    postid INTEGER NOT NULL,
     created DATETIME,
-    FOREIGN KEY (owner) REFERENCES users(username),
-    FOREIGN KEY (postid) REFERENCES posts(postid)
+    FOREIGN KEY (owner) REFERENCES users(username) ON DELETE CASCADE,
+    FOREIGN KEY (postid) REFERENCES posts(postid) ON DELETE CASCADE
 );
