e2168894d03b2abdb73c4417abb7e79877ab0e34
johnkyle <johnkyle@umich.edu>
2022-01-31 22:51:13 -0500

    updating

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -70,20 +70,95 @@ def show_index():
             "logname": logname,
             "posts": posts
         }
+        insta485.model.close_db()
         return flask.render_template("index.html",  **context)
+
+@insta485.app.route('/accounts/login/', methods=['POST'])
+def logout():
+    flask.session.clear()
+    return flask.redirect(flask.url_for('show_index'))
 
 
 @insta485.app.route('/accounts/login/', methods=['POST'])
 def login():
     # POST-only route for handling login requests
-    # TODO: Implement this route
-    print('DEBUG Login:', flask.request.form['username'])
     flask.session['logname'] = flask.request.form['username']
     return flask.redirect(flask.url_for('show_index'))
 
-@insta485.app.route('/var/uploads/<path:filename>')
-def send_file(filename):
-    return flask.send_from_directory("/", filename)
 
-@insta485.app.route('/accounts/create/', methods=['POST'])
-def create():
+@insta485.app.route('/users/<user_url_slug>/', methods=['GET'])
+def show_user(user_url_slug):
+    # open database
+    connection = insta485.model.get_db()
+    connection.row_factory = sqlite3.Row
+    # build the context
+    # logname, username, logname_follows_username, fullname, following, followers
+    # total_posts, posts = [{'postid', 'img_url'}]
+
+    logname = flask.session['logname']
+    username = user_url_slug
+    # check if the user exists
+    usr = connection.execute(
+        "SELECT U.username "
+        "FROM users U "
+        "WHERE U.username = ? ",
+        (username, )
+    ).fetchall()
+    if username not in usr:
+        flask.abort(404)
+    logname_follows_username_tbl = connection.execute(
+        "SELECT F.username1 "
+        "FROM following F "
+        "WHERE ? = F.username1 AND  ? = F.username2 "
+        (logname, username)
+    )
+
+    logname_follows_username = logname in logname_follows_username_tbl
+    fullname = connection.execute(
+        "SELECT U.fullname "
+        "FROM users U "
+        "WHERE U.username = ?",
+        (username,)
+    ).fetchall()
+    fullname = fullname[0]['fullname']
+
+    following = len(connection.execute(
+        "SELECT F.username2 "
+        "FROM following F "
+        "WHERE ? = F.username1 "
+        (username, )
+    ).fetchall())
+
+    followers = len(connection.execute(
+        "SELECT F.username1 "
+        "FROM following F "
+        "WHERE ? = F.username2 "
+        (username, )
+    ).fetchall())
+
+    posts_tbl = connection.execute(
+        "SELECT P.postid, P.filename"
+        "FROM posts P"
+        "WHERE ? = P.owner",
+        (username,)
+    )
+    posts = [{'postid': elt['postid'], 'img_url': elt['filename']} for elt in posts_tbl]
+    total_posts = len(posts)
+    context = {
+        'logname': logname,
+        'username': username,
+        'logname_follows_username': logname_follows_username,
+        'fullname': fullname,
+        'following': following,
+        'followers': followers,
+        'total_posts': total_posts,
+        'posts': posts
+    }
+    return flask.render_template("user.html", **context)
+
+
+
+
+@insta485.app.route('/following/', methods=['POST'])
+def follow_unfollow():
+    target = flask.request.args.get('target')
